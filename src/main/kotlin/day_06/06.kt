2d38d0220aab7db57df5c25109e6d2b3e5e29c05
Lukas Seifriedsberger <games.luki@gmail.com>
2025-12-15 23:49:46 +0100

    improved solution for day 6, added cleaner parsing and Equation data class

--- src/main/kotlin/day_06/06.kt
+++ src/main/kotlin/day_06/06.kt
@@ -1,81 +1,61 @@
 package day_06
 
 import java.io.File
-import java.util.regex.Pattern
 
-fun a(input: List<List<String>>) {
-    val answer = (0..input[0].lastIndex).sumOf { problemIndex ->
-        val values = input.take(input.size - 1).map { it[problemIndex].toLong() }
-        val operation = input.last()[problemIndex]
-
-        println("$values - $operation")
-
-        when (operation) {
-            "+" -> values.slice(1..values.lastIndex).fold(values.first()) { acc, value -> acc + value }
-            "*" -> values.slice(1..values.lastIndex).fold(values.first()) { acc, value -> acc * value }
-            else -> error("Illegal operation")
+data class Equation(val numbers: List<Long>, val operation: String) {
+    fun solve(): Long {
+        return when (operation) {
+            "+" -> numbers.reduce { acc, next -> acc + next }
+            "*" -> numbers.reduce { acc, next -> acc * next }
+            else -> error("Unknown operation $operation")
         }
     }
+}
+
+fun a(numberLines: List<String>, operators: List<String>) {
+    val numbersGrid = numberLines.map { line ->
+        line.trim().split("\\s+".toRegex()).map { it.toLong() }
+    }
+
+    val answer = operators.indices.map { colIndex ->
+        val numbers = numbersGrid.map { row -> row[colIndex] }
+        Equation(numbers, operators[colIndex])
+    }.sumOf { it.solve() }
 
     println("Answer: $answer")
 }
 
-fun b(input: List<List<String>>) {
-    val answer = input.sumOf { problem ->
-        val operation = problem.last().last()
-        val values = problem.dropLast(1).map { it.toLong() }
+fun b(numberLines: List<String>, operators: List<String>) {
+    val charGrid = numberLines.map { it.toCharArray() }
 
-        when (operation) {
-            '+' -> values.drop(1).fold(values.first()) { acc, value -> acc + value }
-            '*' -> values.drop(1).fold(values.first()) { acc, value -> acc * value }
+    val curNumbers = mutableListOf<Long>()
+    val equations = mutableListOf<Equation>()
 
-            else -> error("Illegal operation")
+    for (colIndex in charGrid[0].lastIndex downTo 0) {
+        val col = charGrid.map { it[colIndex] }.joinToString("")
+        if (col.isNotBlank()) {
+            curNumbers += col.trim().toLong()
+        } else {
+            equations.add(Equation(curNumbers.toList(), operators[equations.lastIndex + 1]))
+            curNumbers.clear()
         }
     }
+    equations.add(Equation(curNumbers.toList(), operators[equations.lastIndex + 1]))
+
+    val answer = equations.sumOf { it.solve() }
 
     println("Answer b: $answer")
 }
 
-fun Array<CharArray>.pivot(): Array<CharArray> {
-    return (this[0].lastIndex downTo 0).map { col ->
-        (0..this.lastIndex).map { row ->
-            this[row][col]
-        }.toCharArray()
-    }.toTypedArray()
-}
-
-fun Array<CharArray>.prettyPrint() = joinToString("\n") { it.joinToString("") }
 
 fun main() {
     // val input = "06/example.in"
     val input = "06/6.in"
-
-    val linesA = File(input).readLines().map { line ->
-        line.trim().split(Pattern.compile("\\s+"))
-    }
-
-    println(linesA)
-    a(linesA) // 5060053676136
-
     val lines = File(input).readLines()
 
-    val pivotedNumbers = lines
-        .map { line -> line.toCharArray() }
-        .dropLast(1)
-        .toTypedArray()
-        .pivot()
+    val numbers = lines.dropLast(1)
+    val operators = lines.last().trim().split("\\s+".toRegex())
 
-    val operations = lines.last().trim().split(Pattern.compile("\\s+")).map { it.trim() }.reversed()
-
-    val linesB = pivotedNumbers
-        .prettyPrint()
-        .split(" ".repeat(pivotedNumbers.first().size))
-        .map { l -> l.trim().split("\n").map { it.trim() } }
-        .map { it.toMutableList() }
-
-    val gangster = linesB.withIndex().map { indexedProblem ->
-        indexedProblem.value.apply { add(operations[indexedProblem.index]) }
-    }
-
-    b(gangster) // 9695042567249
+    a(numbers, operators)             // 5060053676136
+    b(numbers, operators.reversed())  // 9695042567249
 }
