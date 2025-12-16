ba4ccec3034b3434ffbee785dadbc92433325f28
Lukas Seifriedsberger <games.luki@gmail.com>
2025-12-16 01:30:25 +0100

    purely functional solution for day 6 as was the original intention

--- src/main/kotlin/day_06/06.kt
+++ src/main/kotlin/day_06/06.kt
@@ -3,44 +3,56 @@ package day_06
 import java.io.File
 
 data class Equation(val numbers: List<Long>, val operation: String) {
-    fun solve(): Long {
-        return when (operation) {
-            "+" -> numbers.reduce { acc, next -> acc + next }
-            "*" -> numbers.reduce { acc, next -> acc * next }
-            else -> error("Unknown operation $operation")
-        }
+    fun solve() = when (operation) {
+        "+" -> numbers.reduce { acc, next -> acc + next }
+        "*" -> numbers.reduce { acc, next -> acc * next }
+        else -> error("Unknown operation $operation")
     }
 }
 
-fun a(numberLines: List<String>, operators: List<String>) {
-    val numbersGrid = numberLines.map { line ->
-        line.trim().split("\\s+".toRegex()).map { it.toLong() }
+fun String.splitByBlank(): List<String> = split("\\s+".toRegex())
+
+fun List<String>.splitByEmpty(): List<List<String>> =
+    this.fold(mutableListOf(mutableListOf<String>())) { acc, element ->
+        if (element.isBlank()) {
+            acc.apply { this += mutableListOf<String>() }
+        } else {
+            acc.apply { last() += element }
+        }
     }
 
-    val answer = operators.indices.map { colIndex ->
-        val numbers = numbersGrid.map { row -> row[colIndex] }
-        Equation(numbers, operators[colIndex])
-    }.sumOf { it.solve() }
+fun List<List<String>>.pivot(): List<List<String>> =
+    this.first().indices.fold(mutableListOf()) { acc, colIndex ->
+        val col = this.map { it[colIndex] }
+        acc.apply { this += col }
+    }
 
-    println("Answer: $answer")
+fun List<String>.pivot(): List<String> =
+    this.first().indices.fold(mutableListOf()) { acc, colIndex ->
+        val col = this.map { it[colIndex] }.joinToString("")
+        acc.apply { this += col }
+    }
+
+fun a(numberLines: List<String>, operators: List<String>) {
+    val problemNumbers = numberLines.map { line -> line.trim().splitByBlank() }.pivot()
+        .map { problem -> problem.map { it.trim().toLong() } }
+
+    val equations = problemNumbers.mapIndexed { index, number ->
+        Equation(number, operators[index])
+    }
+
+    val answer = equations.sumOf { it.solve() }
+
+    println("Answer a: $answer")
 }
 
 fun b(numberLines: List<String>, operators: List<String>) {
-    val charGrid = numberLines.map { it.toCharArray() }
+    val problemNumbers = numberLines.pivot().splitByEmpty().reversed()
+        .map { problem -> problem.map { it.trim().toLong() } }
 
-    val curNumbers = mutableListOf<Long>()
-    val equations = mutableListOf<Equation>()
-
-    for (colIndex in charGrid[0].lastIndex downTo 0) {
-        val col = charGrid.map { it[colIndex] }.joinToString("")
-        if (col.isNotBlank()) {
-            curNumbers += col.trim().toLong()
-        } else {
-            equations.add(Equation(curNumbers.toList(), operators[equations.lastIndex + 1]))
-            curNumbers.clear()
-        }
+    val equations = problemNumbers.mapIndexed { index, numbers ->
+        Equation(numbers, operators[index])
     }
-    equations.add(Equation(curNumbers.toList(), operators[equations.lastIndex + 1]))
 
     val answer = equations.sumOf { it.solve() }
 
@@ -53,9 +65,9 @@ fun main() {
     val input = "06/6.in"
     val lines = File(input).readLines()
 
-    val numbers = lines.dropLast(1)
-    val operators = lines.last().trim().split("\\s+".toRegex())
+    val numberLines = lines.dropLast(1)
+    val operators = lines.last().trim().splitByBlank()
 
-    a(numbers, operators)             // 5060053676136
-    b(numbers, operators.reversed())  // 9695042567249
+    a(numberLines, operators)             // 5060053676136
+    b(numberLines, operators.reversed())  // 9695042567249
 }
